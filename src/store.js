46322139ae7726cf9082b8d4588931fd331a0a49
Jared Rigby <talk@jaredrigby.co.uk>
2021-12-08 15:52:03 +0800

    Created task 2

--- src/store.js
+++ src/store.js
@@ -22,11 +22,12 @@ export default new Vuex.Store({
         solved: false
       },
       {
-        name: "",
+        name: "Who is this?",
         points: 200,
-        detail: "",
-        files: [""],
-        md5: "", // done
+        detail: "I was making some ASCII art but something went wrong, there's supposed to be a new line every 200 characters. Can you tell me who this member of staff is? Give your answer in the format:",
+        monospace: "dcbCTF{firstname_lastname}",
+        files: ["https://raw.githubusercontent.com/dulwichbeijing/igcse-mini-ctf-2/main/public/task-files/task2.txt"],
+        md5: "cd970d0f2191d796cedc111eb60ba5da",
         solved: false
       },
       {
@@ -34,8 +35,8 @@ export default new Vuex.Store({
         points: 200,
         detail: "Only one combination of inputs will have a positive output, format your flag by giving the letter then the binary value. E.g.:",
         monospace: "dcbCTF{a0b0c0d0e0} // if all inputs are 0",
-        files: ["https://raw.githubusercontent.com/dulwichbeijing/igcse-mini-ctf-2/main/public/task-files/task3.jpg"],
-        md5: "", // done
+        files: ["https://raw.githubusercontent.com/dulwichbeijing/igcse-mini-ctf-2/main/public/task-files/task3.png"],
+        md5: "cf5146a3e42808ba5810174ea05077dd",
         solved: false
       },
       {
